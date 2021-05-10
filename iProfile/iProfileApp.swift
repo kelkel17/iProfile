2a3e9a40cf09385d6da3879036e6a6453308781a
micmic17 <mickale_saturre@ligph.com>
2021-05-10 11:07:40 +0800

    Initial Commit

---
+++ iProfile/iProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  iProfileApp.swift
+//  iProfile
+//
+//  Created by Mickale Saturre on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
